b3afe45729c7267fbaadd431ac50482c2f8385a8
Nelessx <nelessxkarn@gmail.com>
2024-12-27 08:24:55 +0545

    map

--- react/firstapp/src/App.js
+++ react/firstapp/src/App.js
@@ -4,77 +4,238 @@ import dashboard from "./Dashboard.png";
 import amazon from "./amazon.png";
 
 export default function App() {
+
+    const student =[
+        {name: "nilesh",
+            image: <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5">
+              <path d="M14 7a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z" />
+              <path
+                stroke-linecap="round"
+                d="M18 10s-3.537 1.5-6 1.5S6 10 6 10m6 2v1.452m0 0a3 3 0 0 0 .476 1.623L15 19m-3-5.548a3 3 0 0 1-.476 1.623L9 19"
+              />
+              <path
+                stroke-linecap="round"
+                d="M7 3.338A9.95 9.95 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12c0-1.821.487-3.53 1.338-5"
+              />
+            </g>
+          </svg>
+        },
+        {
+            name: "hari",
+            image: <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5">
+              <path d="M14 7a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z" />
+              <path
+                stroke-linecap="round"
+                d="M18 10s-3.537 1.5-6 1.5S6 10 6 10m6 2v1.452m0 0a3 3 0 0 0 .476 1.623L15 19m-3-5.548a3 3 0 0 1-.476 1.623L9 19"
+              />
+              <path
+                stroke-linecap="round"
+                d="M7 3.338A9.95 9.95 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12c0-1.821.487-3.53 1.338-5"
+              />
+            </g>
+          </svg>
+        },
+        {
+            name:"ram",
+            image: <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5">
+              <path d="M14 7a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z" />
+              <path
+                stroke-linecap="round"
+                d="M18 10s-3.537 1.5-6 1.5S6 10 6 10m6 2v1.452m0 0a3 3 0 0 0 .476 1.623L15 19m-3-5.548a3 3 0 0 1-.476 1.623L9 19"
+              />
+              <path
+                stroke-linecap="round"
+                d="M7 3.338A9.95 9.95 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12c0-1.821.487-3.53 1.338-5"
+              />
+            </g>
+          </svg>
+        },
+        {
+            name:"ram",
+            image: <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5">
+              <path d="M14 7a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z" />
+              <path
+                stroke-linecap="round"
+                d="M18 10s-3.537 1.5-6 1.5S6 10 6 10m6 2v1.452m0 0a3 3 0 0 0 .476 1.623L15 19m-3-5.548a3 3 0 0 1-.476 1.623L9 19"
+              />
+              <path
+                stroke-linecap="round"
+                d="M7 3.338A9.95 9.95 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12c0-1.821.487-3.53 1.338-5"
+              />
+            </g>
+          </svg>
+        },
+        {
+            name:"ram",
+            image: <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5">
+              <path d="M14 7a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z" />
+              <path
+                stroke-linecap="round"
+                d="M18 10s-3.537 1.5-6 1.5S6 10 6 10m6 2v1.452m0 0a3 3 0 0 0 .476 1.623L15 19m-3-5.548a3 3 0 0 1-.476 1.623L9 19"
+              />
+              <path
+                stroke-linecap="round"
+                d="M7 3.338A9.95 9.95 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12c0-1.821.487-3.53 1.338-5"
+              />
+            </g>
+          </svg>
+        },
+        {
+            name:"ram",
+            image: <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5">
+              <path d="M14 7a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z" />
+              <path
+                stroke-linecap="round"
+                d="M18 10s-3.537 1.5-6 1.5S6 10 6 10m6 2v1.452m0 0a3 3 0 0 0 .476 1.623L15 19m-3-5.548a3 3 0 0 1-.476 1.623L9 19"
+              />
+              <path
+                stroke-linecap="round"
+                d="M7 3.338A9.95 9.95 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12c0-1.821.487-3.53 1.338-5"
+              />
+            </g>
+          </svg>
+        }
+        
+    ]
   return (
     <div>
       <div>
-        <nav className=" bg-[#051216]">
-          <header className="flex items-center justify-between lg:w-10/12 mx-auto   text-white p-2">
-            <img src={logo} alt="" />
+        <div>
+          <nav className=" bg-[#051216]">
+            <header className="flex items-center justify-between lg:w-10/12 mx-auto   text-white p-2">
+              <img src={logo} alt="" />
 
-            <div className=" hidden lg:block">
-              <div className="flex justify-between gap-10 font-semibold ">
-                <p className=" hover:text-orange-500 duration-300 cursor-pointer">
-                  Home
-                </p>
-                <p className=" hover:text-orange-500 duration-300 cursor-pointer">
-                  About
-                </p>
-                <p className=" hover:text-orange-500 duration-300 cursor-pointer">
-                  Services
-                </p>
-                <p className=" hover:text-orange-500 duration-300 cursor-pointer">
-                  Price
-                </p>
-                <p className=" hover:text-orange-500 duration-300 cursor-pointer">
-                  Contact
-                </p>
+              <div className=" hidden lg:block">
+                <div className="flex justify-between gap-10 font-semibold ">
+                  <p className=" hover:text-orange-500 duration-300 cursor-pointer">
+                    Home
+                  </p>
+                  <p className=" hover:text-orange-500 duration-300 cursor-pointer">
+                    About
+                  </p>
+                  <p className=" hover:text-orange-500 duration-300 cursor-pointer">
+                    Services
+                  </p>
+                  <p className=" hover:text-orange-500 duration-300 cursor-pointer">
+                    Price
+                  </p>
+                  <p className=" hover:text-orange-500 duration-300 cursor-pointer">
+                    Contact
+                  </p>
+                </div>
               </div>
+
+              <button className=" bg-orange-600 pl-8 pr-8 pt-3 pb-3 font-semibold rounded-sm ">
+                Login
+              </button>
+            </header>
+          </nav>
+
+          <div className=" lg:w-7/12 mx-auto mt-24 flex flex-col items-center gap-8">
+            <div className=" text-3xl lg:text-6xl text-center font-semibold leading-relaxed">
+              Make Your Business Flourish with{" "}
+              <span className=" text-orange-500">Digital Marketing</span>
             </div>
 
-            <button className=" bg-orange-600 pl-8 pr-8 pt-3 pb-3 font-semibold rounded-sm ">
-              Login
-            </button>
-          </header>
-        </nav>
+            <p className=" text-center">
+              Save time on creating a website from scratch. Use a rich
+              collection of ready-made websites to get your project off to a
+              fast start opposed to using 'Content here, content here', making
+              it look like readable English.
+            </p>
 
-        <div className=" lg:w-7/12 mx-auto mt-24 flex flex-col items-center gap-8">
-          <div className=" text-3xl lg:text-6xl text-center font-semibold leading-relaxed">
-            Make Your Business Flourish with{" "}
-            <span className=" text-orange-500">Digital Marketing</span>
+            <div className=" space-x-8 ">
+              <button className=" bg-slate-950 text-white px-8 py-4 ">
+                Try 30-Day Trial
+              </button>
+              <button className=" border border-slate-950 px-8 py-4">
+                {" "}
+                Schedule a Call
+              </button>
+            </div>
+
+            <img
+              className=" border p-4 rounded-2xl bg-gray-200"
+              src={dashboard}
+              alt=""
+            />
           </div>
+        </div>
 
-          <p className=" text-center">
-            Save time on creating a website from scratch. Use a rich collection
-            of ready-made websites to get your project off to a fast start
-            opposed to using 'Content here, content here', making it look like
-            readable English.
-          </p>
-
-          <div className=" space-x-8 ">
-            <button className=" bg-slate-950 text-white px-8 py-4 ">
-              Try 30-Day Trial
-            </button>
-            <button className=" border border-slate-950 px-8 py-4">
-              {" "}
-              Schedule a Call
-            </button>
-          </div>
-
-          <img
-            className=" border p-4 rounded-2xl bg-gray-200"
-            src={dashboard}
-            alt=""
-          />
+        <div className=" w-8/12 mx-auto grid grid-cols-6 space-x-8 mt-20">
+          <img src={amazon} alt="" />
+          <img src={amazon} alt="" />
+          <img src={amazon} alt="" />
+          <img src={amazon} alt="" />
+          <img src={amazon} alt="" />
+          <img src={amazon} alt="" />
         </div>
       </div>
 
-      <div className=" w-8/12 mx-auto grid grid-cols-6 space-x-8 mt-20">
-        <img src={amazon} alt="" />
-        <img src={amazon} alt="" />
-        <img src={amazon} alt="" />
-        <img src={amazon} alt="" />
-        <img src={amazon} alt="" />
-        <img src={amazon} alt="" />
+      <div>
+
+      </div>
+
+      <div className=" bg-yellow-200 my-11">
+      <div className=" grid grid-cols-2 lg:grid-cols-5 gap-8 w-10/12 mx-auto py-20 bg-yellow-200 ">
+        {
+            student.map((std) => (
+                <div className="bg-slate-500 p-7 w-40 h-40 rounded-2xl flex flex-col justify-around items-center"> 
+                <div>{std.image}</div>
+                <p className="text-lg font-semibold">{std.name}</p>
+                </div>
+            )
+            )
+        }
+      </div>
       </div>
     </div>
   );
